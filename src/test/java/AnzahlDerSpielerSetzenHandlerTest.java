d85b22d931b407654b3c69a3d3451460ecc5d675
Asphilia <janike@katter-ebe.de>
2018-11-23 13:06:23 +0100

    testklassen wieder eingefügt

---
+++ src/test/java/AnzahlDerSpielerSetzenHandlerTest.java
@@ -0,0 +1,19 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnzahlDerSpielerSetzenHandlerTest {
+
+	@Test
+	public void testCanHandle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandle() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/LevelEinstellenHandlerTest.java
@@ -0,0 +1,22 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LevelEinstellenHandlerTest {
+
+	@Test
+	public void testLevelEinstellenHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanHandle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandle() {
+		fail("Not yet implemented");
+	}
+
+}
