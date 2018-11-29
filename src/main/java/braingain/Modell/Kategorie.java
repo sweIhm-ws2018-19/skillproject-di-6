e378779db633fe0702a594511b9d760d74cb8680
SDouglas3 <43849251+SDouglas3@users.noreply.github.com>
2018-11-29 16:36:34 +0100

    Update Kategorie.java

--- src/main/java/braingain/Modell/Kategorie.java
+++ src/main/java/braingain/Modell/Kategorie.java
@@ -1,15 +1,15 @@
 package main.java.braingain.Modell;
 
 enum Kategorie {
-	LOGIK({"logik"}),
+	LOGIK({"logik","logic"}),
 	MATHE({"mathe","mathematik"}),
 	GEOGRAFIE({"geo","geografie","geographie","erdkunde"}),
 	GEDAECHTNISTRAINING({"gedaechtnistraining","gehirntraining"});
 	
-	private String[] Value;
+	private String[] value;
 	
-	Kategorie(String[] kategorien){this.Value = kategorien;}
+	public Kategorie(String[] kategorien){this.value = kategorien;}
 	
-	public String[] Value() {return Value;}
+	public String[] value() {return value;}
 
 }
