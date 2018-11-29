bc87dd10270155204aaf0fc428a673d9abf8aa66
DerPat <43779009+MineRickStar@users.noreply.github.com>
2018-11-29 17:01:36 +0100

    Update FrageTest.java
    Neuen Konstruktor

--- src/test/java/FrageTest.java
+++ src/test/java/FrageTest.java
@@ -4,7 +4,7 @@ import main.java.braingain.Modell.Frage;
 
 public class FrageTest {
 	
-	Frage frage = new Frage("Wie viele Seiten hat ein Wuerfel?");
+	Frage frage = new Frage("Wie viele Seiten hat ein Wuerfel?", "6");
 	
 	//Keine Funktionen bis jetzt
 }
