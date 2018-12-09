171edb0fb212d99c6a40ed13f8fc50c01224b3da
unknown <Patrick.Zann97@gmail.com>
2018-12-09 22:25:35 +0100

    Javadoc hinzugefügt

--- src/main/java/braingain/modell/Spielmodus.java
+++ src/main/java/braingain/modell/Spielmodus.java
@@ -1,15 +1,11 @@
 package braingain.modell;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Enum Spielmodus.
  */
 public enum Spielmodus {
 	
-	/** The alleine. */
 	ALLEINE,
-	
-	/** The duell. */
 	DUELL;
 
 }
