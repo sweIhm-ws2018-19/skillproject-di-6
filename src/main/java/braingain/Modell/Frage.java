ae472c144e6e1a154f85cde635ed99f1540a75f9
SDouglas3 <43849251+SDouglas3@users.noreply.github.com>
2018-12-09 22:58:19 +0100

    Habs wieder rückgängig gemacht - falscher branch

--- src/main/java/braingain/Modell/Frage.java
+++ src/main/java/braingain/Modell/Frage.java
@@ -7,12 +7,6 @@ public class Frage implemets Serializable{
 	
 	private String frage;
 	private ArrayList<String> antworten;
-	public static ArrayList<ArrayList<ArrayList<Frage>>> alleFragen;
-	
-	static{
-		makeNewMap();
-		readQuestions();
-	}
 	
 	public Frage(String frage, String antwort) {
 		this.frage = frage;
@@ -41,49 +35,4 @@ public class Frage implemets Serializable{
 		return antworten;
 	}
 	
-	static void makeNewMap() {
-		fragen = new ArrayList<>(); 
-		for(int i = 0; i< KategorieValues.length; i++) {
-			fragen.add(new ArrayList<>());
-			for(int j = 0 ; j < LevelValues.length ; j++ ) {
-				fragen.get(i).add(new ArrayList<Frage>());
-			}
-		}	
-	}
-	
-	private static void newQuestion(String frage, String antwort, int kat, int level) {
-		fragen.get(kat-1).get(level-1).add(new Frage(frage,antwort));
-	}
-	
-	static void readQuestions() {
-		String fileName = "resources"+ File.separator + "Fragen.txt";
-		
-		try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))){
-			
-			for (String temp = reader.readLine(); temp != null; temp = reader.readLine()) {
-				int zuordnung = Integer.parseInt(temp);
-				int lvl = zuordnung % 10;
-				int kat = (zuordnung-lvl)/10;
-				String Frage = reader.readLine();
-				String Antwort = reader.readLine();
-				newQuestion(Frage,Antwort,kat,lvl);
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//Hilfsfunktion, um die Richtigkeit der Uebernahme zu ueberpruefen
-	private static void iterate() {
-		for(int i = 0; i< KategorieValues.length; i++) {
-			for(int j = 0 ; j < LevelValues.length ; j++ ) {
-				for(Frage f : fragen.get(i).get(j)) {
-					System.out.println(f.getFrage());
-					System.out.println(f.getAntworten());
-								
-				}
-			}
-		}
-	}	
 }
