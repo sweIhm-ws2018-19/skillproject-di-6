1682a7fd48a32e32f3f85638ecd811bfa83eeb8f
unknown <Patrick.Zann97@gmail.com>
2018-12-09 22:19:36 +0100

    Javadoc hinzugefügt

--- src/main/java/braingain/modell/Frage.java
+++ src/main/java/braingain/modell/Frage.java
@@ -2,17 +2,32 @@ package braingain.modell;
 
 import java.util.ArrayList;
 
+/**
+ * The Class Frage.
+ */
 public class Frage {
 	
 	private String frage;
 	private ArrayList<String> antworten;
 	
+	/**
+	 * Initialisiert eine neue Frage, mit einer Frage und einer Antwort als String.
+	 *
+	 * @param frage the frage
+	 * @param antwort the antwort
+	 */
 	public Frage(String frage, String antwort) {
 		this.frage = frage;
 		antworten = new ArrayList<String>();
 		antworten.add(antwort);
 	}
 	
+	/**
+	 * Initialisiert eine neue Frage, mit einem String-Array als Antworten.
+	 *
+	 * @param frage the frage
+	 * @param antworten the antworten
+	 */
 	public Frage(String frage, String[] antworten) {
 		this.frage = frage;
 		this.antworten = new ArrayList<String>();
@@ -21,15 +36,31 @@ public class Frage {
 		}
 	}
 	
+	/**
+	 * Initialisiert eine neue Frage mit einer Array-List als antwort.
+	 *
+	 * @param frage the frage
+	 * @param antworten the antworten
+	 */
 	public Frage(String frage, ArrayList<String> antworten){
 		this.frage = frage;
 		this.antworten = antworten;
 	}
 	
+	/**
+	 * Gets the frage.
+	 *
+	 * @return the frage
+	 */
 	public String getFrage() {
 		return frage;
 	}
 	
+	/**
+	 * Diese Methode sollte aufgerufen werden, wenn es nur eine Antwort gibt.
+	 *
+	 * @return the antwort string
+	 */
 	public String getAntwortString() {
 		if(antworten.size() > 1) {
 			System.out.println("Es gibt mehr als nur eine Antwort, es wird nur die erste zurueckgegeben.");;
@@ -37,15 +68,24 @@ public class Frage {
 		return antworten.get(0);
 	}
 	
-	public ArrayList<String> getAntwortenArrayList(){
-		return antworten;
-	}
-	
+	/**
+	 * Diese Methode sollte aufgerufen werden, wenn man ein String-Array als Antwort haben will.
+	 *
+	 * @return the antworten array
+	 */
 	public String[] getAntwortenArray() {
 		String[] antwortenArray = new String[antworten.size()];
 		for(int i = 0; i < antworten.size(); i++) {
 			antwortenArray[i] = antworten.get(i);
 		}
 		return antwortenArray;
+	}
+	/**
+	 * Mit dieser Methode bekommt man die Array-List als Antwort wieder.
+	 *
+	 * @return the antworten array list
+	 */
+	public ArrayList<String> getAntwortenArrayList(){
+		return antworten;
 	}
 }
--- src/main/java/braingain/modell/Spieler.java
+++ src/main/java/braingain/modell/Spieler.java
@@ -1,11 +1,21 @@
 package braingain.modell;
 
+/**
+ * The Class Spieler.
+ */
 public class Spieler {
 	
 	private final String name;
 	private int punktestand;
 	private int highscore;
 	
+	/**
+	 * Initialisiert einen neuen Spieler, mit Namen, PunkteStand und Highscore.
+	 *
+	 * @param name the name
+	 * @param punktestand the punktestand
+	 * @param highscore the highscore
+	 */
 	public Spieler(String name, int punktestand, int highscore) {
 		
 		this.name = name;
@@ -14,22 +24,45 @@ public class Spieler {
 		
 	}
 
+	/**
+	 * Diese Methode testet, ob man richtig oder falsch geantwortet hat.
+	 *
+	 * @param richtigBeantwortet the richtig beantwortet
+	 */
 	public void beantwortet(boolean richtigBeantwortet){
 		punktestand = richtigBeantwortet ? punktestand + 1 : punktestand - 1;
 	}
 	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Gets the punktestand.
+	 *
+	 * @return the punktestand
+	 */
 	public int getPunktestand() {
 		return punktestand;
 	}
 
+	/**
+	 * Gets the highscore.
+	 *
+	 * @return the highscore
+	 */
 	public int getHighscore() {
 		return highscore;
 	}
 
+	/**
+	 * Sets the highscore.
+	 */
 	public void setHighscore() {
 		if(punktestand > highscore) {
 			highscore = punktestand;
