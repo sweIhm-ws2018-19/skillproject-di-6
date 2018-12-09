ee41b35be607a7cb4a9a66927a45b4a0377105d2
unknown <Patrick.Zann97@gmail.com>
2018-12-09 18:51:43 +0100

    Neue SpielrundeTestKlasse

--- src/test/java/modellTest/SpielrundeTest.java
+++ src/test/java/modellTest/SpielrundeTest.java
@@ -1,14 +1,26 @@
 package modellTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class SpielrundeTest {
+import braingain.modell.Kategorie;
+import braingain.modell.Level;
+import braingain.modell.Spielrunde;
 
+class SpielrundeTest {
+	
+	private static final int ANZAHL_DER_SPIELER = 4;
+	private static final Kategorie KATEGORIE = Kategorie.LOGIK;
+	private static final Level LEVEL = Level.MITTEL;
+	
+	Spielrunde sr = new Spielrunde();
+	
 	@Test
 	void testSpielrunde() {
-		fail("Not yet implemented");
+		Assertions.assertNotNull(sr.spieler, "Der Konstruktor funktioniert nicht.");
+		Assertions.assertNotNull(sr.fragen, "Der Konstruktor funktioniert nicht.");
 	}
 
 	@Test
@@ -28,32 +40,38 @@ class SpielrundeTest {
 
 	@Test
 	void testGetAnzahlSpieler() {
-		fail("Not yet implemented");
+		sr.setAnzahlDerSpieler(ANZAHL_DER_SPIELER);
+		Assertions.assertEquals(ANZAHL_DER_SPIELER, sr.getAnzahlSpieler(), "Der Getter fuer Anzahl der Spieler funktioniert nicht richtig.");
 	}
 
 	@Test
 	void testSetAnzahlDerSpieler() {
-		fail("Not yet implemented");
+		sr.setAnzahlDerSpieler(ANZAHL_DER_SPIELER);
+		Assertions.assertEquals(ANZAHL_DER_SPIELER, sr.getAnzahlSpieler(), "Die Anzahl der Spieler wird nicht gesetzt.");
 	}
 
 	@Test
 	void testSetKategorie() {
-		fail("Not yet implemented");
+		Assertions.assertTrue(sr.setKategorie(KATEGORIE.toString()));
+		Assertions.assertFalse(sr.setKategorie("nonKategorie"), "Die Kategorie nonKategorie gibt es nicht.");
 	}
 
 	@Test
 	void testGetKategorie() {
-		fail("Not yet implemented");
+		sr.setKategorie(KATEGORIE.toString());
+		Assertions.assertEquals(KATEGORIE, sr.getKategorie(KATEGORIE.toString()), "Die Methode getKategorie ist falsch.");
 	}
 
 	@Test
 	void testSetLevel() {
-		fail("Not yet implemented");
+		Assertions.assertTrue(sr.setLevel(LEVEL.toString()));
+		Assertions.assertFalse(sr.setLevel("nonLevel"), "Das Level nonLevel gibt es nicht.");
 	}
 
 	@Test
 	void testGetLevel() {
-		fail("Not yet implemented");
+		sr.setLevel(LEVEL.toString());
+		Assertions.assertEquals(LEVEL, sr.getLevel(LEVEL.toString()), "Die Methode getLevel ist inkorrekt.");
 	}
 
 	@Test
