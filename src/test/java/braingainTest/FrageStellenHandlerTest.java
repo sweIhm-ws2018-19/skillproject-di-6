a359b04788f981794d16e9f61146f40b9c8ff8e4
unknown <Patrick.Zann97@gmail.com>
2018-12-09 22:45:38 +0100

    update

---
+++ src/test/java/braingainTest/FrageStellenHandlerTest.java
@@ -0,0 +1,24 @@
+package braingainTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FrageStellenHandlerTest {
+
+	@Test
+	void testFrageStellenHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCanHandle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHandle() {
+		fail("Not yet implemented");
+	}
+
+}
