77ceeacb202bcbd81040a6568c35ba06c3efcf57
unknown <Patrick.Zann97@gmail.com>
2018-12-10 09:29:22 +0100

    erste Tests geschrieben

--- src/test/java/braingainTest/FrageStellenHandlerTest.java
+++ src/test/java/braingainTest/FrageStellenHandlerTest.java
@@ -1,24 +1,38 @@
 package braingainTest;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.*;
+import com.amazon.ask.model.slu.entityresolution.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import braingain.handlers.FrageStellenHandler;
+import braingain.modell.Frage;
+import braingain.modell.Spielrunde;
 
 class FrageStellenHandlerTest {
 
-	@Test
-	void testFrageStellenHandler() {
-		fail("Not yet implemented");
-	}
+	private FrageStellenHandler handler = new FrageStellenHandler(new Spielrunde());
+	private static final Frage FRAGE = new Frage("Wie heisst die Hauptstadt von Schweden?", "Stockholm");
 
 	@Test
 	void testCanHandle() {
-		fail("Not yet implemented");
+		HandlerInput handlerInputMock = Mockito.mock(HandlerInput.class);
+		Mockito.when(handlerInputMock.matches(any())).thenReturn(true);
+		assertTrue(handler.canHandle(handlerInputMock));
 	}
 
 	@Test
 	void testHandle() {
-		fail("Not yet implemented");
+		HandlerInput handlerInputMock = Mockito.mock(HandlerInput.class);
+		Mockito.when(handlerInputMock.matches(any())).thenReturn(FRAGE.getFrage());
+		Assertions.assertEquals(FRAGE.getFrage(), handler.handle(handlerInputMock));
 	}
 
 }
