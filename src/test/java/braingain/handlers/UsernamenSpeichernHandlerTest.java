0012de94d9734f6d28351634f895310f1e4df021
SDouglas3 <43849251+SDouglas3@users.noreply.github.com>
2018-12-10 20:54:28 +0100

    UsernamenSpeichernHandlerTest Auskommentiert

--- src/test/java/braingain/handlers/UsernamenSpeichernHandlerTest.java
+++ src/test/java/braingain/handlers/UsernamenSpeichernHandlerTest.java
@@ -1,11 +1,11 @@
 package braingain.handlers;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 class UsernamenSpeichernHandlerTest {
-
+/*
 	@Test
 	void testCanHandle() {
 		fail("Not yet implemented");
@@ -15,5 +15,5 @@ class UsernamenSpeichernHandlerTest {
 	void testHandle() {
 		fail("Not yet implemented");
 	}
-
+*/
 }
