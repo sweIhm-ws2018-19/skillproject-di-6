d07f8dc80b231c8ca829f079028b961c809bc7bc
unknown <Patrick.Zann97@gmail.com>
2018-12-10 17:58:37 +0100

    Neue Testklasse

---
+++ src/test/java/braingain/handlers/FrageStellenHandlerTest.java
@@ -0,0 +1,40 @@
+package braingain.handlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+
+import braingain.modell.Frage;
+import braingain.modell.Spielrunde;
+
+class FrageStellenHandlerTest {
+	
+	private FrageStellenHandler handler = new FrageStellenHandler(new Spielrunde());
+	private static final Frage FRAGE = new Frage("Wie heisst die Hauptstadt von Schweden?", "Stockholm");
+
+	@Test
+	void testCanHandle() {
+		HandlerInput handlerInputMock = Mockito.mock(HandlerInput.class);
+		Mockito.when(handlerInputMock.matches(any())).thenReturn(true);
+		assertTrue(handler.canHandle(handlerInputMock));
+	}
+
+	@Test
+	void testHandle() {
+		HandlerInput handlerInputMock = Mockito.mock(HandlerInput.class);
+		
+		Response response = handler.handle(handlerInputMock).get();
+		
+		Assertions.assertFalse(response.getShouldEndSession());
+		
+		Mockito.when(handlerInputMock.getResponseBuilder().withSpeech(FRAGE.getFrage()));
+		Assertions.assertEquals(FRAGE.getFrage(), handler.handle(handlerInputMock));
+	}
+
+}
