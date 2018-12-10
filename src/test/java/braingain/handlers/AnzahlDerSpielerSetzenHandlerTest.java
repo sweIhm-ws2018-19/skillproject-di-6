1196c9889146cd4ec51503c39dedf6c52f461ad1
unknown <Patrick.Zann97@gmail.com>
2018-12-10 17:58:15 +0100

    update

--- src/test/java/braingain/handlers/AnzahlDerSpielerSetzenHandlerTest.java
+++ src/test/java/braingain/handlers/AnzahlDerSpielerSetzenHandlerTest.java
@@ -1,14 +1,24 @@
 package braingain.handlers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+
+import braingain.modell.Spielrunde;
 
 class AnzahlDerSpielerSetzenHandlerTest {
-
+	
+	private AnzahlDerSpielerSetzenHandler handler = new AnzahlDerSpielerSetzenHandler(new Spielrunde());
+	
 	@Test
 	void testCanHandle() {
-		fail("Not yet implemented");
+		HandlerInput handlerInputMock = Mockito.mock(HandlerInput.class);
+		Mockito.when(handlerInputMock.matches(any())).thenReturn(true);
+		assertTrue(handler.canHandle(handlerInputMock));
 	}
 
 	@Test
