db37f312bf1d6bc5122c65c54b4b1d6b1b84b2bd
Asphilia <43779157+Asphilia@users.noreply.github.com>
2018-12-10 21:46:13 +0100

    Auskommentiert

--- src/main/java/braingain/streamHandler/BraingainStreamHandler.java
+++ src/main/java/braingain/streamHandler/BraingainStreamHandler.java
@@ -45,8 +45,8 @@ private static Skill getSkill() {
 				new LevelEinstellenHandler(sr),
 				new SessionEndedRequestHandler(sr),
 				new UsernamenSpeichernHandler(sr))
-				.withTableName("HighScore")
-				.withAutoCreateTable(true)
+				//.withTableName("HighScore")
+				//.withAutoCreateTable(true)
 				.withSkillId("amzn1.ask.skill.9a1dd27b-4aa6-4e19-a454-5e4525eab49b")
 				.build();
 }
