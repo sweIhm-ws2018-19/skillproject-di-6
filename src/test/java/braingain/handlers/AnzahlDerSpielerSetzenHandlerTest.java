f67c426d40f816dffb93feac58567970a9efbf4a
SDouglas3 <43849251+SDouglas3@users.noreply.github.com>
2018-12-10 20:43:05 +0100

    Auskommentiertes AnzahlDerSpielerSetzenTest II

--- src/test/java/braingain/handlers/AnzahlDerSpielerSetzenHandlerTest.java
+++ src/test/java/braingain/handlers/AnzahlDerSpielerSetzenHandlerTest.java
@@ -9,9 +9,9 @@ import org.mockito.Mockito;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 
 import braingain.modell.Spielrunde;
-
+*/
 class AnzahlDerSpielerSetzenHandlerTest {
-	
+	/*
 	private AnzahlDerSpielerSetzenHandler handler = new AnzahlDerSpielerSetzenHandler(new Spielrunde());
 	
 	@Test
