dc676d5583274fa53e7c201723ec0f2221c7103e
unknown <Patrick.Zann97@gmail.com>
2018-12-10 09:27:49 +0100

    Konstruktor public gemacht

--- src/main/java/braingain/handlers/FrageStellenHandler.java
+++ src/main/java/braingain/handlers/FrageStellenHandler.java
@@ -1,35 +1,32 @@
-package main.java.colorpicker.handlers;
+package braingain.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
 
-import Modell.Spielrunde;
+import braingain.modell.Spielrunde;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
 
 public class FrageStellenHandler implements RequestHandler {
-	
+
 	private Spielrunde sr;
 
-	FrageStellenHandler(Spielrunde sr){
-		this.sr=sr;
+	public FrageStellenHandler(Spielrunde sr) {
+		this.sr = sr;
 	}
-	
-	
-    @Override
-    public boolean canHandle(HandlerInput input) {
-        return input.matches(intentName("FrageStellenIntent"));
-    }
 
-    @Override
-    public Optional<Response> handle(HandlerInput input) {
-        String speechText=sr.getFragen();
+	@Override
+	public boolean canHandle(HandlerInput input) {
+		return input.matches(intentName("FrageStellenIntent"));
+	}
 
-        return input.getResponseBuilder()
-                .withSpeech(speechText)
-                .withSimpleCard("FrageSession", speechText).withSpeech(speechText).withShouldEndSession(false)
-                .build();
-    }
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		String speechText = sr.fetchFrage();
+
+		return input.getResponseBuilder().withSpeech(speechText).withSimpleCard("FrageSession", speechText)
+				.withSpeech(speechText).withShouldEndSession(false).build();
+	}
 }
