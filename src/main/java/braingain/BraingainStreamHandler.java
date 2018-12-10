e0612bbfb4b672822f18b64ddcd2697f6e1ca070
unknown <Patrick.Zann97@gmail.com>
2018-12-10 23:10:10 +0100

    braingain.streamhandler update

--- src/main/java/braingain/BraingainStreamHandler.java
+++ src/main/java/braingain/BraingainStreamHandler.java
@@ -10,7 +10,7 @@ or in the "license" file accompanying this file. This file is distributed on an 
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
 the specific language governing permissions and limitations under the License.
 */
-package braingain.streamHandler;
+package braingain;
 
 
 import com.amazon.ask.Skill;
