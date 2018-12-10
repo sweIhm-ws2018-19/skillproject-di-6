a8d3a94700363fe39b411732fee2bc626b900f37
unknown <Patrick.Zann97@gmail.com>
2018-12-10 22:38:39 +0100

    neue TestKlasse

---
+++ src/test/java/braingain/handlers/AntwortHandlerTest.java
@@ -0,0 +1,20 @@
+package braingain.handlers;
+
+/*import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+*/
+
+class AntwortHandlerTest {
+
+/*	@Test
+	void testCanHandle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHandle() {
+		fail("Not yet implemented");
+	}
+*/
+}
