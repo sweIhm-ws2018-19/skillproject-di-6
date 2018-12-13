f635b9b705de8a178ac21a1136f4f876412b37a7
unknown <Patrick.Zann97@gmail.com>
2018-12-13 21:04:55 +0100

    update mit speechtext und reprompttext

--- src/main/java/braingain/handlers/AntwortHandler.java
+++ src/main/java/braingain/handlers/AntwortHandler.java
@@ -13,6 +13,7 @@ import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.Request;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.model.Slot;
+import com.amazon.ask.response.ResponseBuilder;
 
 import braingain.modell.Spielrunde;
 
@@ -31,31 +32,32 @@ public class AntwortHandler implements RequestHandler {
 
 	public Optional<Response> handle(HandlerInput input) {
 
-		String speechText;
+		String speechText, repromptText;
 		Request request = input.getRequestEnvelope().getRequest();
 		IntentRequest intentRequest = (IntentRequest) request;
 		Intent intent = intentRequest.getIntent();
 		Map<String, Slot> slots = intent.getSlots();
-
-		Slot antwortSlot = slots.get(ANTWORT);
 		
+		ResponseBuilder responseBuilder = input.getResponseBuilder();
+		
+		Slot antwortSlot = slots.get(ANTWORT);
+
 		if (antwortSlot != null) {
 			String antwort = antwortSlot.getValue();
 			input.getAttributesManager().setSessionAttributes(Collections.singletonMap(antwort, ANTWORT));
-			boolean richtig = sr.checkAntwort(antwort);
-			if(richtig) {
-			speechText = String.format("Deine Antwort war richtig!");
-			}else {
-				speechText = String.format("Deine Antwort war leider falsch. Die richtige Antwort ist %s", sr.getRichtigeAntwort());
+			
+			if (sr.checkAnswer(antwort)) {
+				speechText = "Deine Antwort ist richtig!";
+			} else {
+				speechText = String.format("Deine Antwort war leider falsch. Die richtige Antwort ist %s",
+						sr.getRightAnswer());
 			}
-
+			responseBuilder.withSimpleCard("Anwser", speechText).withSpeech(speechText).withShouldEndSession(false);
 		} else {
-			speechText = "Ich habe deine Antwort leider nicht verstanden. Bitte wiederhole deine Antwort.";
+			repromptText = "Ich habe deine Antwort leider nicht verstanden. Bitte wiederhole deine Antwort.";
+			responseBuilder.withSimpleCard("Answer", repromptText).withSpeech(repromptText).withShouldEndSession(false);
+		}
+		return responseBuilder.build();
+	}
 
-		}
-			return input.getResponseBuilder().withSpeech(speechText)
-					.withShouldEndSession(false)
-					.build();
-		}
-	
 }
