e8c33f2ad3c9a7ea4e340163992dcf535b57f4de
unknown <Patrick.Zann97@gmail.com>
2018-12-13 21:08:56 +0100

    refactor und test ob es Level gibt

--- src/main/java/braingain/handlers/LevelEinstellenHandler.java
+++ src/main/java/braingain/handlers/LevelEinstellenHandler.java
@@ -38,35 +38,39 @@ public class LevelEinstellenHandler implements RequestHandler {
 
 		Slot selectedLevelSlot = slots.get(LIST_OF_LEVEL);
 		ResponseBuilder responseBuilder = input.getResponseBuilder();
-		
+
 		String speechText, repromptText;
 
 		if (selectedLevelSlot != null) {
-			if (sr.getAnzahlSpieler() == 1) {
-				String gewaehltesLevel = selectedLevelSlot.getValue();
-				input.getAttributesManager()
-						.setSessionAttributes(Collections.singletonMap(gewaehltesLevel, LIST_OF_LEVEL));
 
-				speechText = String.format("Du hast das Level %s gewaehlt. Nun werden dir 10 Fragen gestellt. Sage Los, um zu beginnen.",
-						gewaehltesLevel);
-				sr.setLevel(gewaehltesLevel);
-				sr.refreshFragen();
-			} else {
-				String gewaehltesLevel = selectedLevelSlot.getValue();
-				input.getAttributesManager()
-						.setSessionAttributes(Collections.singletonMap(gewaehltesLevel, LIST_OF_LEVEL));
+			String gewaehltesLevel = selectedLevelSlot.getResolutions().getResolutionsPerAuthority().get(0).getValues()
+					.get(0).getValue().getName();
 
-				speechText = String.format("Ihr habt das Level %s gewaehlt. Euch werden nun "+ sr.getAnzahlSpieler() * 5 +" Fragen gestellt",
-						gewaehltesLevel);
-				sr.setLevel(gewaehltesLevel);
+			input.getAttributesManager().setSessionAttributes(Collections.singletonMap(gewaehltesLevel, LIST_OF_LEVEL));
+			if (sr.setLevel(gewaehltesLevel)) {
+				if (sr.getNumberOfPlayers() == 1) {
+					speechText = String.format(
+							"Du hast das Level %s gewaehlt. Dir werden nun 10 Fragen gestellt. Sage Los, um zu beginnen.",
+							sr.getLevel().toString());
+				} else {
+					speechText = String.format("Ihr habt das Level %s gewaehlt. Euch werden nun "
+							+ sr.getNumberOfPlayers() * 5 + " Fragen gestellt. Sagt los um zu beginnen.",
+							sr.getLevel().toString());
+				}
 				sr.buildQuestions();
+				responseBuilder.withSimpleCard("LevelSession", speechText).withSpeech(speechText)
+						.withShouldEndSession(false);
+			} else {
+				repromptText = String.format("Das Level %s kenne ich nicht.", gewaehltesLevel);
+				responseBuilder.withSimpleCard("LevelSession", repromptText).withReprompt(repromptText)
+						.withShouldEndSession(false);
 			}
-			responseBuilder.withSimpleCard("LevelSession", speechText).withSpeech(speechText).withShouldEndSession(false);
 		} else {
 			repromptText = "Ich habe das Level nicht verstanden. Sage mir das Level, in welchem du abgefragt werden willst. Sage zum Beispiel: Mittel.";
-			responseBuilder.withSimpleCard("LevelSession", repromptText).withReprompt(repromptText).withShouldEndSession(false);
+			responseBuilder.withSimpleCard("LevelSession", repromptText).withReprompt(repromptText)
+					.withShouldEndSession(false);
 		}
-		
+
 		return responseBuilder.build();
 	}
 
