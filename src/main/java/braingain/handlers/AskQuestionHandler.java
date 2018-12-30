ea69d692fec5a775625f587ca42063eded1320ff
unknown <Patrick.Zann97@gmail.com>
2018-12-30 14:00:47 +0100

    Test added if everything is set

--- src/main/java/braingain/handlers/AskQuestionHandler.java
+++ src/main/java/braingain/handlers/AskQuestionHandler.java
@@ -1,36 +1,60 @@
 package braingain.handlers;
 
-import com.amazon.ask.dispatcher.request.handler.HandlerInput;
-import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.Response;
-
-import braingain.modell.Spielrunde;
-import phrasesAndConstants.PhrasesAndConstants;
+import static com.amazon.ask.request.Predicates.intentName;
 
 import java.util.Optional;
 
-import static com.amazon.ask.request.Predicates.intentName;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.response.ResponseBuilder;
 
-public class FrageStellenHandler implements RequestHandler {
+import braingain.modell.Category;
+import braingain.modell.Gameround;
+import phrasesAndConstants.PhrasesAndConstants;
 
-	private LaunchRequestHandler lrh;
-	private Spielrunde round;
+public class AskQuestionHandler implements RequestHandler {
 
-	public FrageStellenHandler(LaunchRequestHandler lrh) {
-		this.lrh = lrh;
-		round = lrh.round;
+	private Gameround round;
+
+	public AskQuestionHandler(Gameround round) {
+		this.round = round;
 	}
 
 	@Override
 	public boolean canHandle(HandlerInput input) {
-		return input.matches(intentName("FrageStellenIntent"));
+		return input.matches(intentName(PhrasesAndConstants.INTENT_ASK_QUESTION));
 	}
 
 	@Override
 	public Optional<Response> handle(HandlerInput input) {
 		String speechText = "No Questions available yet.";
 
-		return input.getResponseBuilder().withSpeech(speechText).withSimpleCard(PhrasesAndConstants.CARD_TITLE, speechText)
-				.withShouldEndSession(false).build();
+		ResponseBuilder responseBuilder = input.getResponseBuilder();
+
+		if (round.isEverythingSet()) {
+			round.buildQuestions();
+			// TODO Everything is set, now start the questions
+			
+			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, speechText).withSpeech(speechText);
+
+		} else if (round.getNumberOfPlayers() == 0) {
+			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.SET_NUMBER_OF_PLAYERS)
+					.withSpeech(PhrasesAndConstants.SET_NUMBER_OF_PLAYERS);
+		} else if (!round.allPlayerSet()) {
+			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.SET_PLAYER_NAMES)
+					.withSpeech(PhrasesAndConstants.SET_PLAYER_NAMES);
+		} else if (round.getCategory() == null) {
+			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.SET_CATEGORY)
+					.withSpeech(PhrasesAndConstants.SET_CATEGORY);
+		} else if (round.getLevel() == null && round.getCategory() != Category.KOFFERPACKEN) {
+			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.SET_LEVEL)
+					.withSpeech(PhrasesAndConstants.SET_LEVEL);
+		} else {
+			// TODO What to do, when nothing is given in the handlerinput
+		}
+
+		return responseBuilder.withShouldEndSession(false).build();
 	}
+
 }
