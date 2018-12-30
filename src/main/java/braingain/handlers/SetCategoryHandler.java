fbf05196a1dfc6f41556d7d34276d1dfe178e14d
unknown <Patrick.Zann97@gmail.com>
2018-12-30 14:02:55 +0100

    Added Koffer packen as Category

--- src/main/java/braingain/handlers/SetCategoryHandler.java
+++ src/main/java/braingain/handlers/SetCategoryHandler.java
@@ -12,67 +12,47 @@ import com.amazon.ask.model.Response;
 import com.amazon.ask.model.Slot;
 import com.amazon.ask.response.ResponseBuilder;
 
-import braingain.modell.Level;
-import braingain.modell.Spielrunde;
+import braingain.modell.Category;
+import braingain.modell.Gameround;
 import phrasesAndConstants.PhrasesAndConstants;
 
-public class KategorieEinstellenHandler implements RequestHandler {
+public class SetCategoryHandler implements RequestHandler {
 
-	private static final Object LIST_OF_CATEGORIES = "gewaehlteKategorie";
-	private LaunchRequestHandler lrh;
-	private Spielrunde round;
+	private Gameround round;
 
-	public KategorieEinstellenHandler(LaunchRequestHandler lrh) {
-		this.lrh = lrh;
-		this.round = lrh.round;
+	public SetCategoryHandler(Gameround round) {
+		this.round = round;
 	}
 
 	public boolean canHandle(HandlerInput input) {
-		return input.matches(intentName("KategorieEinstellenIntent"));
+		return input.matches(intentName(PhrasesAndConstants.INTENT_SET_CATEGORY));
 	}
 
 	public Optional<Response> handle(HandlerInput input) {
-		String speechText, repromptText;
-
+		String speechText;
 		Slot selectedCathegorySlot = ((IntentRequest) input.getRequestEnvelope().getRequest()).getIntent().getSlots()
-				.get(LIST_OF_CATEGORIES);
-
+				.get(PhrasesAndConstants.LIST_OF_CATEGORIES);
 		ResponseBuilder responseBuilder = input.getResponseBuilder();
 
-		if (selectedCathegorySlot != null && round.getNumberOfPlayers() != 0 && round.allPlayersSet()
-				&& round.getCategory() == null) {
-
-			String gewaehlteKategorie = selectedCathegorySlot.getResolutions().getResolutionsPerAuthority().get(0)
+		if (selectedCathegorySlot != null) {
+			String choosencategoryString = selectedCathegorySlot.getResolutions().getResolutionsPerAuthority().get(0)
 					.getValues().get(0).getValue().getName();
-			input.getAttributesManager()
-					.setSessionAttributes(Collections.singletonMap(gewaehlteKategorie, LIST_OF_CATEGORIES));
-			if (round.setKategorie(gewaehlteKategorie)) {
-				speechText = round.getNumberOfPlayers() == 1 ? "Du hast " : "Ihr habt ";
-				speechText += String.format("die Kategorie %s gewaehlt. ", round.getCategory().toString());
-				speechText += round.getNumberOfPlayers() == 1 ? "Waehle " : "Waehlt";
-				speechText += " nun das Level. Es gibt " + Level.getLevels();
-
-				responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, speechText).withSpeech(speechText)
-						.withShouldEndSession(false);
+			input.getAttributesManager().setSessionAttributes(
+					Collections.singletonMap(choosencategoryString, PhrasesAndConstants.LIST_OF_CATEGORIES));
+			round.setCategory(Category.getCategory(choosencategoryString));
+			speechText = round.getNumberOfPlayers() == 1 ? "Du hast " : "Ihr habt ";
+			speechText += String.format("die Kategorie %s gewaehlt. ", round.getCategory().toString());
+			if (round.getCategory() == Category.KOFFERPACKEN) {
+				speechText += PhrasesAndConstants.START_BACK_PACKING;
 			} else {
-				repromptText = String.format("Die %s kenne ich nicht, bitte erneut eingeben", gewaehlteKategorie);
-				responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, repromptText).withSpeech(repromptText)
-						.withShouldEndSession(false);
+				speechText += PhrasesAndConstants.LIST_ALL_LEVELS;
 			}
-		} else if (round.getNumberOfPlayers() == 0) {
-			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.SET_NUMBER_OF_PLAYERS)
-					.withSpeech(PhrasesAndConstants.SET_NUMBER_OF_PLAYERS).withShouldEndSession(false);
-		} else if (!round.allPlayersSet()) {
-			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.SET_PLAYER_NAMES)
-					.withSpeech(PhrasesAndConstants.SET_PLAYER_NAMES).withShouldEndSession(false);
-		} else if (round.getCategory() != null) {
-			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.RESET_CATEGORY)
-					.withSpeech(PhrasesAndConstants.RESET_CATEGORY).withShouldEndSession(false);
+			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, speechText).withSpeech(speechText);
 		} else {
 			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.REPROMPT_CATEGORY)
-					.withSpeech(PhrasesAndConstants.REPROMPT_CATEGORY).withShouldEndSession(false);
+					.withSpeech(PhrasesAndConstants.REPROMPT_CATEGORY);
 		}
-		return responseBuilder.build();
+		return responseBuilder.withShouldEndSession(false).build();
 	}
 
 }
