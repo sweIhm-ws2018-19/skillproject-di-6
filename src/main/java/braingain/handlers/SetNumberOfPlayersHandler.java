affde9c25b561a8653603b9588a820d8301a800a
unknown <Patrick.Zann97@gmail.com>
2018-12-30 14:03:36 +0100

    refactor

--- src/main/java/braingain/handlers/SetNumberOfPlayersHandler.java
+++ src/main/java/braingain/handlers/SetNumberOfPlayersHandler.java
@@ -25,52 +25,50 @@ import com.amazon.ask.model.Response;
 import com.amazon.ask.model.Slot;
 import com.amazon.ask.response.ResponseBuilder;
 
-import braingain.modell.Spielrunde;
+import braingain.modell.Gameround;
 import phrasesAndConstants.PhrasesAndConstants;
 
-public class AnzahlDerSpielerSetzenHandler implements RequestHandler {
+public class SetNumberOfPlayersHandler implements RequestHandler {
 
+	private Gameround round;
 
-	private LaunchRequestHandler lrh;
-	private Spielrunde round;
-
-	public AnzahlDerSpielerSetzenHandler(LaunchRequestHandler lrh) {
-		this.lrh = lrh;
-		round = lrh.round;
+	public SetNumberOfPlayersHandler(Gameround round) {
+		this.round = round;
 	}
 
 	@Override
 	public boolean canHandle(HandlerInput input) {
-		return input.matches(intentName("AnzahlDerSpielerSetzenIntent"));
+		return input.matches(intentName(PhrasesAndConstants.INTENT_SET_NUMBER_OF_PLAYERS));
 	}
 
 	@Override
 	public Optional<Response> handle(HandlerInput input) {
-		String speechText;
-	
 		Slot selectedPlayerSlot = ((IntentRequest) input.getRequestEnvelope().getRequest()).getIntent().getSlots()
 				.get(PhrasesAndConstants.LIST_OF_PLAYERNUMBERS);
-
 		ResponseBuilder responseBuilder = input.getResponseBuilder();
-		
+
 		if (selectedPlayerSlot != null) {
 			String numberOfPlayers = selectedPlayerSlot.getResolutions().getResolutionsPerAuthority().get(0).getValues()
 					.get(0).getValue().getName();
-			input.getAttributesManager()
-					.setSessionAttributes(Collections.singletonMap(numberOfPlayers, PhrasesAndConstants.LIST_OF_PLAYERNUMBERS));
+			input.getAttributesManager().setSessionAttributes(
+					Collections.singletonMap(numberOfPlayers, PhrasesAndConstants.LIST_OF_PLAYERNUMBERS));
 			round.setNumberOfPlayers(Integer.parseInt(numberOfPlayers));
 			if (round.getNumberOfPlayers() == 1) {
-				speechText = PhrasesAndConstants.PLAY_ALONE;
+				round.setMaxQuestions(PhrasesAndConstants.MAX_NUMBER_OF_QUESTIONS_ONE_PLAYER);
+				responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.PLAY_ALONE)
+						.withSpeech(PhrasesAndConstants.PLAY_ALONE);
 			} else {
-				speechText = "OK. Ihr spielt nun zu " + round.getNumberOfPlayers() + " ." + PhrasesAndConstants.PLAY_WITH_MORE;
+				round.setMaxQuestions(round.getNumberOfPlayers() * PhrasesAndConstants.MAX_NUMBER_OF_QUESTIONS_MORE_PLAYER);
+				String speechText = String.format("OK. Ihr spielt nun zu %s. %s", round.getNumberOfPlayers(),
+						PhrasesAndConstants.PLAY_WITH_MORE);
+				responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.PLAY_WITH_MORE)
+						.withSpeech(speechText);
 			}
-			responseBuilder.withSimpleCard(PhrasesAndConstants.CARD_TITLE, speechText).withSpeech(speechText)
-					.withShouldEndSession(false);
 		} else {
 			responseBuilder
 					.withSimpleCard(PhrasesAndConstants.CARD_TITLE, PhrasesAndConstants.REPROMPT_NUMBER_OF_PLAYERS)
-					.withSpeech(PhrasesAndConstants.REPROMPT_NUMBER_OF_PLAYERS).withShouldEndSession(false);
+					.withSpeech(PhrasesAndConstants.REPROMPT_NUMBER_OF_PLAYERS);
 		}
-		return responseBuilder.build();
+		return responseBuilder.withShouldEndSession(false).build();
 	}
 }
