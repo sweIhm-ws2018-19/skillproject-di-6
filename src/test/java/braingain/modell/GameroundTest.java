af6591b41baa0fc75631e9dbfe4c540fa19e6c3f
MineRickStar <Patrick.Zann97@gmail.com>
2019-01-04 14:57:30 +0100

    new Testclass

---
+++ src/test/java/braingain/modell/GameroundTest.java
@@ -0,0 +1,199 @@
+package braingain.modell;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameroundTest {
+//
+//	@Test
+//	public void testGameround() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAddPlayer() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetNextRandomCurrentPlayer() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCheckAnswer() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetRightAnswer() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAddItemToBackPack() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetBackPackingAt() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCheckWord() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetRandomBackPackWordForAlexa() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testBackPackingAlexa() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetPlayerArrayList() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetPlayerArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetPlayerAt() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAllPlayerSet() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetCurrentPlayer() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetHighscore() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetBackPackHighscore() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetPlayersCounted() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testIncreasePlayerCount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetNumberOfPlayers() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetNumberOfPlayers() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetCategory() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetCategory() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetMemoryTraining() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetMemoryTraining() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetLevel() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetLevel() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testIncreaseQuestionsAsked() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetQuestionsAsked() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetMaxQuestions() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetMaxQuestions() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetBackPackSize() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetCurrentQuestion() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetRandomNextCurrentQuestion() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testBuildQuestions() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetPlayerNames() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testIsEverythingSet() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testReset() {
+//		fail("Not yet implemented");
+//	}
+
+}
